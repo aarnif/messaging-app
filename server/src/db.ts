462ee2d3b36c80fe3b2460a11790ae75471ee279
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-09-25 22:40:52 +0300

    Add connection to database

---
+++ server/src/db.ts
@@ -0,0 +1,19 @@
+import { Sequelize } from "sequelize";
+import config from "../config";
+
+const sequelize = new Sequelize(config.DATABASE_URL, {
+  dialect: "postgres",
+});
+
+const connectToDatabase = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+connectToDatabase();
+
+export { sequelize, connectToDatabase };
