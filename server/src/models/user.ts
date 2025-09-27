c80651e22a5ea135126918aae7fedfbaf69d521b
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-09-27 11:52:58 +0300

    Create User-model

---
+++ server/src/models/user.ts
@@ -0,0 +1,45 @@
+import type { InferAttributes, InferCreationAttributes } from "sequelize";
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../db";
+
+class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
+  declare id?: number;
+  declare username: string;
+  declare passwordHash: string;
+  declare name: string;
+  declare avatar: string | null;
+}
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    passwordHash: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    avatar: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: true,
+    modelName: "user",
+  }
+);
+
+export { User };
