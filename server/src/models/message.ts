dcb5063b21de9580e66fca27be4c54c579022785
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-09-27 11:55:36 +0300

    Create Message-model

---
+++ server/src/models/message.ts
@@ -0,0 +1,50 @@
+import type { InferAttributes, InferCreationAttributes } from "sequelize";
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../db";
+
+class Message extends Model<
+  InferAttributes<Message>,
+  InferCreationAttributes<Message>
+> {
+  declare id?: number;
+  declare senderId: number;
+  declare chatId: number;
+  declare content: string;
+}
+Message.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    senderId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "users",
+        key: "id",
+      },
+    },
+    chatId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "chats",
+        key: "id",
+      },
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: true,
+    modelName: "message",
+  }
+);
+
+export { Message };
