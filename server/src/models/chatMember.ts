bd5113485595de11f1045fe6fb8366fc8e082c02
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-09-27 11:55:22 +0300

    Create ChatMember-model

---
+++ server/src/models/chatMember.ts
@@ -0,0 +1,51 @@
+import type { InferAttributes, InferCreationAttributes } from "sequelize";
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../db";
+
+class ChatMember extends Model<
+  InferAttributes<ChatMember>,
+  InferCreationAttributes<ChatMember>
+> {
+  declare id?: number;
+  declare userId: number;
+  declare chatId: number;
+  declare role: "member" | "admin";
+}
+ChatMember.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "users",
+        key: "id",
+      },
+    },
+    chatId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "chats",
+        key: "id",
+      },
+    },
+    role: {
+      type: DataTypes.ENUM("member", "admin"),
+      defaultValue: "member",
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: true,
+    modelName: "chat_member",
+  }
+);
+
+export { ChatMember };
