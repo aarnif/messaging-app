eef2e405d6aad9627352e61f47a14c97fd116beb
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-09-27 11:55:03 +0300

    Create Chat-model

---
+++ server/src/models/chat.ts
@@ -0,0 +1,53 @@
+import type { InferAttributes, InferCreationAttributes } from "sequelize";
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../db";
+
+class Chat extends Model<InferAttributes<Chat>, InferCreationAttributes<Chat>> {
+  declare id?: number;
+  declare type: "private" | "group";
+  declare name: string | null;
+  declare description: string | null;
+  declare avatar: string | null;
+  declare createdBy: number;
+}
+Chat.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    type: {
+      type: DataTypes.ENUM("private", "group"),
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    avatar: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    createdBy: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "users",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    underscored: true,
+    timestamps: true,
+    modelName: "chat",
+  }
+);
+
+export { Chat };
