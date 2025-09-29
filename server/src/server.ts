3d128fda80b0b42e4a89909324c069b93c0b997c
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-09-29 15:46:44 +0300

    Add current user as context to server

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,8 @@ import { gql } from "graphql-tag";
 import { resolvers } from "./resolvers";
 import { connectToDatabase } from "./db";
 import config from "config";
+import jwt from "jsonwebtoken";
+import { User } from "./models";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -22,6 +24,20 @@ const start = async () => {
   const server = new ApolloServer({ typeDefs, resolvers });
   const { url } = await startStandaloneServer(server, {
     listen: { port: config.PORT },
+    context: async ({ req }) => {
+      const auth = req ? req.headers.authorization : null;
+      let currentUser = null;
+      if (auth && auth.startsWith("Bearer ")) {
+        const decodedToken = jwt.verify(auth.substring(7), config.JWT_SECRET);
+        if (
+          typeof decodedToken === "object" &&
+          typeof decodedToken.id === "number"
+        ) {
+          currentUser = await User.findByPk(decodedToken.id);
+        }
+      }
+      return { currentUser };
+    },
   });
   console.log(`Server is now running at ${url}`);
 };
