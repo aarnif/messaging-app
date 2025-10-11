974b53a52d74cae366293d8ab597eac385dbcd4f
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-10-11 22:29:05 +0300

    Add Apollo Client setup

--- ui/src/main.tsx
+++ ui/src/main.tsx
@@ -1,13 +1,17 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router } from "react-router";
+import { ApolloProvider } from "@apollo/client/react";
+import client from "./client.ts";
 import "./index.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ApolloProvider client={client}>
+      <Router>
+        <App />
+      </Router>
+    </ApolloProvider>
   </StrictMode>
 );
