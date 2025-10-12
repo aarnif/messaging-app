0b70d6fedba8bb6922d585477781b97f0cc53d8f
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-10-12 11:16:48 +0300

    Add authorization header to apollo client

--- ui/src/client.ts
+++ ui/src/client.ts
@@ -1,9 +1,23 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
+import { SetContextLink } from "@apollo/client/link/context";
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000",
+});
+
+const authLink = new SetContextLink(({ headers }) => {
+  const token = localStorage.getItem("token");
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 
 const client = new ApolloClient({
-  link: new HttpLink({
-    uri: "http://localhost:4000",
-  }),
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
