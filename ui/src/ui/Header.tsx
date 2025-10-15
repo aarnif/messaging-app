ba824f76393bd6bf4ba47154c583fd5820ac65d6
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-10-15 14:41:15 +0300

    Adjust Header background breakpoint from lg to sm

--- ui/src/ui/Header.tsx
+++ ui/src/ui/Header.tsx
@@ -1,8 +1,8 @@
 const Header = ({ showBackground = false }: { showBackground: boolean }) => (
   <header
-    className={`flex items-center justify-center px-4 py-2 ${showBackground && "lg:bg-slate-100 lg:dark:bg-slate-900"}`}
+    className={`flex items-center justify-center px-4 py-2 ${showBackground && "sm:bg-slate-100 sm:dark:bg-slate-900"}`}
   >
-    <h1 className="text-2xl font-black text-green-600 font-londrina dark:text-green-500">
+    <h1 className="font-londrina text-2xl font-black text-green-600 dark:text-green-500">
       Messaging App
     </h1>
   </header>
