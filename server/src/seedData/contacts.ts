95ab5cd63cb272a090031501d7d5fc1999401a86
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-10-16 17:01:08 +0300

    Add User 1 to all contacts in seed data

--- server/src/seedData/contacts.ts
+++ server/src/seedData/contacts.ts
@@ -1,136 +1,58 @@
 import type { Contact } from "../types/other";
 
 const contacts: Contact[] = [
-  {
-    userId: 1,
-    contactId: 2,
-    isBlocked: false,
-  },
-  {
-    userId: 1,
-    contactId: 3,
-    isBlocked: false,
-  },
-  {
-    userId: 1,
-    contactId: 4,
-    isBlocked: true,
-  },
-  {
-    userId: 2,
-    contactId: 1,
-    isBlocked: true,
-  },
-  {
-    userId: 2,
-    contactId: 3,
-    isBlocked: false,
-  },
-  {
-    userId: 2,
-    contactId: 5,
-    isBlocked: false,
-  },
-  {
-    userId: 3,
-    contactId: 1,
-    isBlocked: false,
-  },
-  {
-    userId: 3,
-    contactId: 2,
-    isBlocked: false,
-  },
-  {
-    userId: 3,
-    contactId: 6,
-    isBlocked: true,
-  },
-  {
-    userId: 4,
-    contactId: 5,
-    isBlocked: false,
-  },
-  {
-    userId: 4,
-    contactId: 7,
-    isBlocked: false,
-  },
-  {
-    userId: 5,
-    contactId: 2,
-    isBlocked: false,
-  },
-  {
-    userId: 5,
-    contactId: 4,
-    isBlocked: false,
-  },
-  {
-    userId: 5,
-    contactId: 8,
-    isBlocked: false,
-  },
-  {
-    userId: 6,
-    contactId: 7,
-    isBlocked: false,
-  },
-  {
-    userId: 6,
-    contactId: 9,
-    isBlocked: false,
-  },
-  {
-    userId: 7,
-    contactId: 4,
-    isBlocked: false,
-  },
-  {
-    userId: 7,
-    contactId: 6,
-    isBlocked: false,
-  },
-  {
-    userId: 7,
-    contactId: 10,
-    isBlocked: false,
-  },
-  {
-    userId: 8,
-    contactId: 5,
-    isBlocked: false,
-  },
-  {
-    userId: 8,
-    contactId: 11,
-    isBlocked: false,
-  },
-  {
-    userId: 9,
-    contactId: 6,
-    isBlocked: false,
-  },
-  {
-    userId: 9,
-    contactId: 10,
-    isBlocked: false,
-  },
-  {
-    userId: 10,
-    contactId: 7,
-    isBlocked: false,
-  },
-  {
-    userId: 10,
-    contactId: 9,
-    isBlocked: false,
-  },
-  {
-    userId: 11,
-    contactId: 8,
-    isBlocked: false,
-  },
+  { userId: 1, contactId: 2, isBlocked: false },
+  { userId: 1, contactId: 3, isBlocked: false },
+  { userId: 1, contactId: 4, isBlocked: false },
+  { userId: 1, contactId: 5, isBlocked: false },
+  { userId: 1, contactId: 6, isBlocked: false },
+  { userId: 1, contactId: 7, isBlocked: false },
+  { userId: 1, contactId: 8, isBlocked: false },
+  { userId: 1, contactId: 9, isBlocked: false },
+  { userId: 1, contactId: 10, isBlocked: false },
+  { userId: 1, contactId: 11, isBlocked: false },
+
+  { userId: 2, contactId: 1, isBlocked: true },
+  { userId: 2, contactId: 1, isBlocked: false },
+  { userId: 2, contactId: 3, isBlocked: false },
+  { userId: 2, contactId: 5, isBlocked: false },
+
+  { userId: 3, contactId: 1, isBlocked: false },
+  { userId: 3, contactId: 2, isBlocked: false },
+  { userId: 3, contactId: 6, isBlocked: false },
+
+  { userId: 4, contactId: 1, isBlocked: false },
+  { userId: 4, contactId: 5, isBlocked: false },
+  { userId: 4, contactId: 7, isBlocked: false },
+
+  { userId: 5, contactId: 1, isBlocked: false },
+  { userId: 5, contactId: 2, isBlocked: false },
+  { userId: 5, contactId: 4, isBlocked: false },
+  { userId: 5, contactId: 8, isBlocked: false },
+
+  { userId: 6, contactId: 1, isBlocked: false },
+  { userId: 6, contactId: 7, isBlocked: false },
+  { userId: 6, contactId: 9, isBlocked: false },
+
+  { userId: 7, contactId: 1, isBlocked: false },
+  { userId: 7, contactId: 4, isBlocked: false },
+  { userId: 7, contactId: 6, isBlocked: false },
+  { userId: 7, contactId: 10, isBlocked: false },
+
+  { userId: 8, contactId: 1, isBlocked: false },
+  { userId: 8, contactId: 5, isBlocked: false },
+  { userId: 8, contactId: 11, isBlocked: false },
+
+  { userId: 9, contactId: 1, isBlocked: false },
+  { userId: 9, contactId: 6, isBlocked: false },
+  { userId: 9, contactId: 10, isBlocked: false },
+
+  { userId: 10, contactId: 1, isBlocked: false },
+  { userId: 10, contactId: 7, isBlocked: false },
+  { userId: 10, contactId: 9, isBlocked: false },
+
+  { userId: 11, contactId: 1, isBlocked: false },
+  { userId: 11, contactId: 8, isBlocked: false },
 ];
 
 export default contacts;
