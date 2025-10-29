e470f2974336284857eb23105b1656f8e8c15813
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-10-29 12:35:17 +0200

    Implement edit profile on profile page

--- ui/src/components/Profile.tsx
+++ ui/src/components/Profile.tsx
@@ -1,18 +1,110 @@
 import { useNavigate, useLocation } from "react-router";
-import { IoChevronBack } from "react-icons/io5";
+import { useMutation } from "@apollo/client/react";
+import { useState } from "react";
+import { IoChevronBack, IoChevronForward } from "react-icons/io5";
+import { MdClose } from "react-icons/md";
 import { FiEdit } from "react-icons/fi";
 import type { User } from "../__generated__/graphql";
+import { EDIT_PROFILE } from "../graphql/mutations";
+import useNotifyMessage from "../hooks/useNotifyMessage";
+import useField from "../hooks/useField";
+import { motion, AnimatePresence } from "motion/react";
+import Notify from "../ui/Notify";
+import FormField from "../ui/FormField";
+
+const EditProfileModal = ({
+  currentUser,
+  setIsEditProfileOpen,
+}: {
+  currentUser: User;
+  setIsEditProfileOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const { name, about } = currentUser;
+  const { message, showMessage } = useNotifyMessage();
+  const nameInput = useField("name", "text", "Enter your name here...", name);
+  const aboutInput = useField(
+    "about",
+    "text",
+    "Tell something about yourself...",
+    about ?? ""
+  );
+
+  const [mutate] = useMutation(EDIT_PROFILE, {
+    onError: (error) => {
+      console.log(error);
+    },
+  });
+
+  const handleEditProfile = async () => {
+    if (nameInput.value.length < 3) {
+      showMessage("Profile name must be at least three characters long");
+      return;
+    }
+
+    await mutate({
+      variables: {
+        input: {
+          name: nameInput.value,
+          about: aboutInput.value,
+          is24HourClock: currentUser.is24HourClock,
+        },
+      },
+    });
+
+    setIsEditProfileOpen(false);
+  };
+
+  return (
+    <motion.div
+      initial={{ x: "100vw" }}
+      animate={{ x: 0 }}
+      exit={{ x: "100vw" }}
+      transition={{ type: "tween", duration: 0.3 }}
+      className="absolute inset-0 flex flex-grow flex-col items-center gap-4 overflow-y-auto bg-white px-2 py-4 sm:gap-8 dark:bg-slate-800"
+    >
+      <div className="flex w-full items-center justify-between">
+        <button
+          data-testid="close-edit-profile-button"
+          className="cursor-pointer"
+          onClick={() => setIsEditProfileOpen(false)}
+        >
+          <MdClose className="h-6 w-6 fill-current text-slate-700 hover:text-slate-900 dark:text-slate-100 dark:hover:text-slate-300" />
+        </button>
+        <h2 className="font-oswald text-2xl font-medium text-slate-900 dark:text-slate-50">
+          Edit Profile
+        </h2>
+
+        <button
+          data-testid="submit-edit-profile-button"
+          className="cursor-pointer"
+          onClick={handleEditProfile}
+        >
+          <IoChevronForward className="h-6 w-6 text-slate-700 hover:text-slate-900 dark:text-slate-100 dark:hover:text-slate-300" />
+        </button>
+      </div>
+      <div className="flex w-full flex-col gap-4">
+        <AnimatePresence>
+          {message && <Notify message={message} />}
+        </AnimatePresence>
+        <FormField field={nameInput} />
+        <FormField field={aboutInput} />
+      </div>
+    </motion.div>
+  );
+};
 
 const Profile = ({ currentUser }: { currentUser: User }) => {
   const navigate = useNavigate();
   const location = useLocation();
   const showListOnMobile = location.pathname === "/settings/profile";
 
+  const [isEditProfileOpen, setIsEditProfileOpen] = useState(false);
+
   const { name, username, about } = currentUser;
 
   return (
     <div
-      className={`flex flex-grow flex-col items-center justify-center bg-white dark:bg-slate-800 ${
+      className={`relative flex flex-grow flex-col items-center justify-center bg-white dark:bg-slate-800 ${
         showListOnMobile ? "" : "hidden sm:flex"
       }`}
     >
@@ -31,7 +123,7 @@ const Profile = ({ currentUser }: { currentUser: User }) => {
           <button
             data-testid="edit-profile-button"
             className="absolute right-2 cursor-pointer"
-            onClick={() => console.log("Edit profile button clicked")}
+            onClick={() => setIsEditProfileOpen(true)}
           >
             <FiEdit className="h-6 w-6 text-slate-700 hover:text-slate-900 dark:text-slate-100 dark:hover:text-slate-300" />
           </button>
@@ -56,6 +148,15 @@ const Profile = ({ currentUser }: { currentUser: User }) => {
           </div>
         </div>
       </div>
+      <AnimatePresence>
+        {isEditProfileOpen && (
+          <EditProfileModal
+            key={"edit-profile"}
+            currentUser={currentUser}
+            setIsEditProfileOpen={setIsEditProfileOpen}
+          />
+        )}
+      </AnimatePresence>
     </div>
   );
 };
--- ui/src/tests/Profile.test.tsx
+++ ui/src/tests/Profile.test.tsx
@@ -1,8 +1,15 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import { describe, test, expect, vi } from "vitest";
 import userEvent from "@testing-library/user-event";
+import { MockedProvider } from "@apollo/client/testing/react";
+import { useMutation } from "@apollo/client/react";
 import { MemoryRouter } from "react-router";
-import { currentUserMock, mockNavigate } from "./mocks";
+import { currentUserMock, editProfile24h, mockNavigate } from "./mocks";
 import Profile from "../components/Profile";
 
 vi.mock("react-router", async () => {
@@ -14,13 +21,28 @@ vi.mock("react-router", async () => {
   };
 });
 
+const mockEditProfile = vi.fn();
+
+vi.mock("@apollo/client/react", async () => {
+  const actual = await vi.importActual("@apollo/client/react");
+  return {
+    ...actual,
+    useMutation: vi.fn(() => [
+      mockEditProfile,
+      { loading: false, error: null },
+    ]),
+  };
+});
+
 const { name, username } = currentUserMock;
 
-const renderComponent = () =>
+const renderComponent = (mocks = [editProfile24h]) =>
   render(
-    <MemoryRouter>
-      <Profile currentUser={currentUserMock} />
-    </MemoryRouter>
+    <MockedProvider mocks={mocks}>
+      <MemoryRouter>
+        <Profile currentUser={currentUserMock} />
+      </MemoryRouter>
+    </MockedProvider>
   );
 
 describe("<Profile />", () => {
@@ -49,6 +71,169 @@ describe("<Profile />", () => {
 
     await waitFor(() => {
       expect(mockNavigate).toHaveBeenCalledWith("/settings");
+    });
+  });
+
+  test("shows edit profile modal when edit profile button is clicked", async () => {
+    const user = userEvent.setup();
+    renderComponent();
+
+    await waitFor(async () => {
+      expect(screen.getByRole("heading", { name: "Profile" })).toBeDefined();
+      expect(screen.getByText(name)).toBeDefined();
+      expect(screen.getByText(`@${username}`)).toBeDefined();
+    });
+
+    await user.click(screen.getByTestId("edit-profile-button"));
+
+    await waitFor(async () => {
+      expect(
+        screen.getByRole("heading", { name: "Edit Profile" })
+      ).toBeDefined();
+      expect(
+        screen.getByPlaceholderText("Enter your name here...")
+      ).toBeDefined();
+      expect(
+        screen.getByPlaceholderText("Tell something about yourself...")
+      ).toBeDefined();
+    });
+  });
+
+  test("closes edit profile modal when close button is clicked", async () => {
+    const user = userEvent.setup();
+    renderComponent();
+
+    await waitFor(async () => {
+      expect(screen.getByRole("heading", { name: "Profile" })).toBeDefined();
+      expect(screen.getByText(name)).toBeDefined();
+      expect(screen.getByText(`@${username}`)).toBeDefined();
+    });
+
+    await user.click(screen.getByTestId("edit-profile-button"));
+
+    await waitFor(async () => {
+      expect(
+        screen.getByRole("heading", { name: "Edit Profile" })
+      ).toBeDefined();
+      expect(
+        screen.getByPlaceholderText("Enter your name here...")
+      ).toBeDefined();
+      expect(
+        screen.getByPlaceholderText("Tell something about yourself...")
+      ).toBeDefined();
+    });
+
+    await user.click(screen.getByTestId("close-edit-profile-button"));
+
+    await waitFor(async () => {
+      expect(
+        screen.queryByRole("heading", { name: "Edit Profile" })
+      ).toBeNull();
+    });
+  });
+
+  test("edit profile fails with empty name", async () => {
+    const user = userEvent.setup();
+    renderComponent();
+
+    await waitFor(async () => {
+      expect(screen.getByRole("heading", { name: "Profile" })).toBeDefined();
+      expect(screen.getByText(name)).toBeDefined();
+      expect(screen.getByText(`@${username}`)).toBeDefined();
+    });
+
+    await user.click(screen.getByTestId("edit-profile-button"));
+
+    const nameInput = screen.getByPlaceholderText("Enter your name here...");
+
+    await waitFor(async () => {
+      expect(
+        screen.getByRole("heading", { name: "Edit Profile" })
+      ).toBeDefined();
+      expect(nameInput).toBeDefined();
+      expect(
+        screen.getByPlaceholderText("Tell something about yourself...")
+      ).toBeDefined();
+    });
+
+    user.clear(nameInput);
+
+    await user.click(screen.getByTestId("submit-edit-profile-button"));
+
+    await waitFor(() => {
+      expect(
+        screen.getByText("Profile name must be at least three characters long")
+      ).toBeDefined();
+    });
+
+    await waitForElementToBeRemoved(
+      () =>
+        screen.queryByText(
+          "Profile name must be at least three characters long"
+        ),
+      { timeout: 3500 }
+    );
+  });
+
+  test("edits profile name and about text succesfully and closes modal", async () => {
+    const mockEditProfile = vi.fn();
+    vi.mocked(useMutation).mockReturnValue([
+      mockEditProfile,
+      {
+        data: undefined,
+        loading: false,
+        error: undefined,
+        called: false,
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        client: {} as any,
+        reset: vi.fn(),
+      },
+    ]);
+
+    const user = userEvent.setup();
+    renderComponent();
+
+    await waitFor(async () => {
+      expect(screen.getByRole("heading", { name: "Profile" })).toBeDefined();
+      expect(screen.getByText(name)).toBeDefined();
+      expect(screen.getByText(`@${username}`)).toBeDefined();
+    });
+
+    await user.click(screen.getByTestId("edit-profile-button"));
+
+    const nameInput = screen.getByPlaceholderText("Enter your name here...");
+    const aboutInput = screen.getByPlaceholderText(
+      "Tell something about yourself..."
+    );
+
+    await waitFor(async () => {
+      expect(
+        screen.getByRole("heading", { name: "Edit Profile" })
+      ).toBeDefined();
+      expect(nameInput).toBeDefined();
+      expect(aboutInput).toBeDefined();
+    });
+
+    const newProfileName = "New Profile Name";
+    const newAboutText = "New About Text";
+
+    await user.clear(nameInput);
+    await user.clear(aboutInput);
+    await user.type(nameInput, newProfileName);
+    await user.type(aboutInput, newAboutText);
+
+    await user.click(screen.getByTestId("submit-edit-profile-button"));
+
+    await waitFor(() => {
+      expect(mockEditProfile).toHaveBeenCalledWith({
+        variables: {
+          input: {
+            name: newProfileName,
+            about: newAboutText,
+            is24HourClock: currentUserMock.is24HourClock,
+          },
+        },
+      });
     });
   });
 });
