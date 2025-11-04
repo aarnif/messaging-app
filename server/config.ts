f7996dafe3d683f3bd447f0216a9a1a418bc0bd8
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-11-04 23:00:36 +0200

    Add SERVER_URL const to config.ts

--- server/config.ts
+++ server/config.ts
@@ -3,5 +3,6 @@ import "dotenv/config";
 const DATABASE_URL = process.env.DATABASE_URL || "";
 const JWT_SECRET = process.env.JWT_SECRET || "";
 const PORT = Number(process.env.PORT) || 4000;
+const SERVER_URL = `http://localhost:${PORT}`;
 
-export default { DATABASE_URL, JWT_SECRET, PORT };
+export default { DATABASE_URL, JWT_SECRET, PORT, SERVER_URL };
