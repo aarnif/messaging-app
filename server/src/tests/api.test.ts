ab662c3c5289cca3729f3b5c2e0843f43655ba90
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-11-05 22:13:14 +0200

    Remove unused variables from count documents server test

--- server/src/tests/api.test.ts
+++ server/src/tests/api.test.ts
@@ -70,12 +70,7 @@ void describe("GraphQL API", () => {
   void test("returns document count from database", async () => {
     const responseBody = await query<{ countDocuments: number }, object>(
       COUNT_DOCUMENTS,
-      {
-        input: {
-          ...user1Input,
-          username: "us",
-        },
-      }
+      {}
     );
     const count = responseBody.data?.countDocuments;
     assert.strictEqual(count, 0);
