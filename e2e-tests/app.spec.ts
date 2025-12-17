d17cb9513a5b6f1b332155559d6f6656eecf5f0f
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-12-17 11:52:19 +0200

    Add e2e test: prevent user creation with invalid username

--- e2e-tests/app.spec.ts
+++ e2e-tests/app.spec.ts
@@ -1,8 +1,30 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("App", () => {
-  test("has correct page title", async ({ page }) => {
+  test.beforeEach(async ({ page, request }) => {
     await page.goto("http://localhost:5173");
+  });
+
+  test("has correct page title", async ({ page }) => {
     await expect(page).toHaveTitle(/Messaging App/);
+  });
+
+  test("prevents user creation with invalid username", async ({ page }) => {
+    await page.getByText("Sign Up").click();
+
+    await page.getByRole("textbox", { name: "Username" }).fill("u");
+    await page
+      .getByRole("textbox", { name: "Password", exact: true })
+      .fill("password");
+
+    await page
+      .getByRole("textbox", { name: "Confirm Password" })
+      .fill("password");
+
+    await page.getByRole("button", { name: "Sign Up" }).click();
+
+    await expect(
+      page.getByText("Username must be at least 3 characters long")
+    ).toBeVisible();
   });
 });
