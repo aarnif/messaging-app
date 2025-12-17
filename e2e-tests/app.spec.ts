2227dc63f67b5914e6bed3e0df01e65035789d07
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-12-17 14:58:31 +0200

    Add test helper: user1

--- e2e-tests/app.spec.ts
+++ e2e-tests/app.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { signUp } from "./helpers/funcs";
+import { user1 } from "./helpers/data";
 
 test.describe("App", () => {
   test.beforeEach(async ({ page, request }) => {
@@ -20,7 +21,7 @@ test.describe("App", () => {
   });
 
   test("prevents user creation with invalid username", async ({ page }) => {
-    await signUp(page, "u", "password", "password");
+    await signUp(page, "u", user1.password, user1.confirmPassword);
 
     await expect(
       page.getByText("Username must be at least 3 characters long")
@@ -28,7 +29,7 @@ test.describe("App", () => {
   });
 
   test("prevents user creation with invalid password", async ({ page }) => {
-    await signUp(page, "user1", "passw", "password");
+    await signUp(page, user1.username, "passw", user1.confirmPassword);
 
     await expect(
       page.getByText("Password must be at least 6 characters long")
@@ -38,26 +39,26 @@ test.describe("App", () => {
   test("prevents user creation with non-matching passwords", async ({
     page,
   }) => {
-    await signUp(page, "user1", "password", "passwor");
+    await signUp(page, user1.username, user1.password, "passwor");
 
     await expect(page.getByText("Passwords do not match")).toBeVisible();
   });
 
   test("can create a new user", async ({ page }) => {
-    await signUp(page, "user1", "password", "password");
+    await signUp(page, user1.username, user1.password, user1.confirmPassword);
     await expect(
       page.getByText("Select Chat to Start Messaging.")
     ).toBeVisible();
   });
 
   test("prevents creating duplicate user", async ({ page, request }) => {
-    await signUp(page, "user1", "password", "password");
+    await signUp(page, user1.username, user1.password, user1.confirmPassword);
 
     await page.getByTestId("logout-button").click();
     await page.getByRole("button", { name: "Logout" }).click();
     await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
 
-    await signUp(page, "user1", "password", "password");
+    await signUp(page, user1.username, user1.password, user1.confirmPassword);
     await expect(page.getByText("Username already exists")).toBeVisible();
   });
 });
---
+++ e2e-tests/helpers/data.ts
@@ -0,0 +1,5 @@
+export const user1 = {
+  username: "user1",
+  password: "password",
+  confirmPassword: "password",
+};
