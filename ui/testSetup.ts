927033b8eabf1ebb8f6451d43b28626f212cd63b
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2025-12-21 22:25:11 +0200

    Disable framer motion animations in component tests

--- ui/testSetup.ts
+++ ui/testSetup.ts
@@ -2,6 +2,10 @@ import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
 import { GraphQLError } from "graphql";
+import { MotionGlobalConfig } from "framer-motion";
+
+// Disable Framer Motion animations in component tests
+MotionGlobalConfig.skipAnimations = true;
 
 afterEach(() => {
   cleanup();
