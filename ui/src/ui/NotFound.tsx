e74098a0a4345d6b82233973a9cd88cd3db3814f
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2026-01-05 10:27:55 +0200

    Use canonical tailwind css class names in NotFound

--- ui/src/ui/NotFound.tsx
+++ ui/src/ui/NotFound.tsx
@@ -1,5 +1,5 @@
 const NotFound = ({ entity }: { entity: string }) => (
-  <div className="flex flex-grow flex-col justify-center">
+  <div className="flex grow flex-col justify-center">
     <p className="text-center font-bold text-red-600">{entity} not found.</p>
     <p className="text-center font-bold text-red-600">
       It may have been deleted or the link is incorrect.
