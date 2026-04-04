10dde2e994fbfae755a30ef967b88112aa72efd6
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2026-04-04 12:22:40 +0300

    Extract NewChat page components into separate files
    Extract NewChatContent and NewMessageBox.

---
+++ frontend/src/pages/NewChat/NewChatContent.tsx
@@ -0,0 +1,41 @@
+import type { User } from "../../__generated__/graphql";
+import type { NewChatDetails } from "../../types";
+import NotFound from "../../components/ui/NotFound";
+import ChatHeader from "../../components/ui/ChatHeader";
+import NewMessageBox from "./NewMessageBox";
+
+const NewChatContent = ({
+  currentUser,
+  chat,
+}: {
+  currentUser: User;
+  chat: NewChatDetails;
+}) => {
+  if (!chat) {
+    return <NotFound entity="Chat" />;
+  }
+
+  const { name, description, members } = chat;
+  const type = members.length == 2 ? null : "group";
+
+  return (
+    <>
+      <ChatHeader
+        type={type}
+        name={name}
+        members={members}
+        currentUser={currentUser}
+      />
+      <div className="grow"></div>
+      <NewMessageBox
+        chatName={name ?? null}
+        chatDescription={description ?? null}
+        chatMembers={
+          members?.filter((member) => member?.id !== currentUser.id) ?? []
+        }
+      />
+    </>
+  );
+};
+
+export default NewChatContent;
