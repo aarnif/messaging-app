7bbd6f31c0430607e6eba78c04c83b3563fe5c89
Aarni Fagerström <aarni.fagerstrom@gmail.com>
2026-05-24 17:23:21 +0300

    Replace empty message console.log with modal in NewMessageBox
    Replace console logging with an alert modal when trying to send an empty
    message in chat.

--- frontend/src/tests/Chat.test.tsx
+++ frontend/src/tests/Chat.test.tsx
@@ -302,15 +302,13 @@ describe("<Chat />", () => {
   });
 
   test("does not send message when input is empty", async () => {
-    const consoleLogSpy = vi.spyOn(console, "log");
-
     const user = userEvent.setup();
     renderComponent();
 
     await sendNewMessage(user, "");
 
     await waitFor(() => {
-      expect(consoleLogSpy).toHaveBeenCalledWith("Do not send empty message!");
+      expect(screen.getByText("Do not send empty message!")).toBeDefined();
     });
   });
 
